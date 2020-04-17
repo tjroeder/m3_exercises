b47dfd075831243406d6d0a8b96f7a1b59f3b649
Sal Espinosa <sespinos@gmail.com>
2020-04-17 17:45:16 -0600

    Add draft solution

--- refactoring/lib/credit_card.rb
+++ refactoring/lib/credit_card.rb
@@ -9,25 +9,26 @@ class CreditCard
   def valid?
     array = @number.split("")
     reversed = array.reverse
-    doubled = reversed.map.with_index do |num, index|
-      if index % 2 == 0
-        num.to_i
-      else
-        num.to_i * 2
-      end
+    doubled = double(reversed)
+    summed = sum_greater_than_ten(doubled)
+    check(summed.sum)
+  end
+
+  private
+  def double(nums)
+    nums.map.with_index do |string, index|
+      num = string.to_i
+      index % 2 == 0 ? num : num * 2
     end
-    summed = doubled.map do |num|
-      if num > 9
-        num - 9
-      else
-        num
-      end
+  end
+
+  def sum_greater_than_ten(array)
+    array.map do |num|
+      num > 9 ? num - 9 : num
     end
-    total = summed.sum
-    if total % 10 == 0
-      true
-    else
-      false
-    end
+  end
+
+  def check(total)
+    total % 10 == 0 ? true : false
   end
 end
