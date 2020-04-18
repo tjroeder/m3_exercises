adb344d61a925e0cb4b35143db85b5a78701e56e
Sal Espinosa <sespinos@gmail.com>
2020-04-17 19:25:33 -0600

    Add solution with no reek complaints

--- refactoring/lib/credit_card.rb
+++ refactoring/lib/credit_card.rb
@@ -1,3 +1,28 @@
+# Monkey patching Integer so I can get rid of utility methods
+
+class Integer
+  def sum_digits
+    self > 9 ? self - 9 : self
+  end
+end
+
+# Monkey patching Array so that I can get rid of utility methods
+
+class Array
+  def double_every_other
+    map.with_index do |string, index|
+      num = string.to_i
+      index % 2 == 0 ? num : num * 2
+    end
+  end
+
+  def sum_greater_than_ten
+    map do |num|
+      num.sum_digits
+    end
+  end
+end
+
 # This CreditCard class is intended to give a simple way to store and validate
 # credit card numbers.
 
@@ -7,28 +32,9 @@ class CreditCard
   end
 
   def valid?
-    array = @number.split("")
-    reversed = array.reverse
-    doubled = double(reversed)
-    summed = sum_greater_than_ten(doubled)
-    check(summed.sum)
-  end
-
-  private
-  def double(nums)
-    nums.map.with_index do |string, index|
-      num = string.to_i
-      index % 2 == 0 ? num : num * 2
-    end
-  end
-
-  def sum_greater_than_ten(array)
-    array.map do |num|
-      num > 9 ? num - 9 : num
-    end
-  end
-
-  def check(total)
-    total % 10 == 0 ? true : false
+    reversed = @number.split("").reverse
+    doubled = reversed.double_every_other
+    summed = doubled.sum_greater_than_ten.sum
+    summed % 10 == 0 ? true : false
   end
 end
