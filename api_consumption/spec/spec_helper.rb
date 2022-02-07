e9ae91048df00c1cd8dc715a2b6ddf962fa70eae
Tim Roeder <timothyjroeder@gmail.com>
2022-02-06 19:43:14 -0700

    Feat: update to use Faraday, Net_http, json, and vcr

--- api_consumption/spec/spec_helper.rb
+++ api_consumption/spec/spec_helper.rb
@@ -1,5 +1,20 @@
 require 'rspec'
 require 'dotenv'
+require 'json'
+require 'pry'
+require 'vcr'
+require 'faraday'
+require 'faraday/net_http'
+
+Faraday.default_adapter = :net_http
+
 Dotenv.load
 
 Dir["./lib/*.rb"].each { |file| require file }
+
+VCR.configure do |c|
+  c.cassette_library_dir = "spec/vcr"
+  c.hook_into :faraday
+  c.configure_rspec_metadata!
+  # c.define_cassette_placeholder("<API_KEY>", ENV["API_KEY"])
+end
